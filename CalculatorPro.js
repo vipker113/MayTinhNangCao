52b6c88cb24b018b6610b435c8e612a0dd54918d
vipker113 <79317962+vipker113@users.noreply.github.com>
2021-07-20 16:06:34 +0700

    Create CalculatorPro.js

---
+++ CalculatorPro.js
@@ -0,0 +1,13 @@
+let view = document.getElementById("view")
+
+    function inputValue(value) {
+        view.value += value;
+    }
+    function result() {
+        let result = eval(view.value);
+        view.value = result;
+    }
+    function xoa() {
+        view.value=""
+
+    }
